64d3ce2130f9f3ce31824b4d6cb74a7aac202e38
Vladimir Vyazovetskov <vlad.vyazovetskov@ostrovok.ru>
2016-12-18 17:56:10 +0300

    Fix.

--- app/bot.py
+++ app/bot.py
@@ -9,10 +9,6 @@ bot = telebot.TeleBot(config.token)
 
 SICKIGRAM_BASE_URL = 'http://www.sickigram.com/'
 
-# @bot.message_handler(content_types=["text"])
-# def repeat_all_messages(message):
-#
-#     bot.send_message(message.chat.id, message.text)
 
 @bot.message_handler(commands=['joke'])
 def get_best_random_joke_from_sgm(message):
@@ -23,12 +19,6 @@ def get_best_random_joke_from_sgm(message):
     best_joke = jokes[0]
     joke = "{title}\n\n{text}".format(**best_joke)
     bot.send_message(message.chat.id, joke)
-
-
-@bot.message_handler(commands=['time'])
-def get_utc_time(message):
-    utc_time = requests.get('http://www.timeapi.org/utc/now').content
-    bot.send_message(message.chat.id, utc_time)
 
 if __name__ == '__main__':
     bot.polling()
